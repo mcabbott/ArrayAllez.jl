44bac11e1c72b0c27bc367639839fc707b7da4af
Michael Abbott <improbable@gmail.com>
2019-01-11 10:50:55 +0100

    flux PR

--- test/runtests.jl
+++ test/runtests.jl
@@ -68,4 +68,30 @@ end
 	# @test gradcheck(z -> sum(iscale_(m,z)), r)
 
 end
+@testset "prod + cumprod" begin # https://github.com/FluxML/Flux.jl/pull/524
 
+	@test gradtest(x -> prod(x, dims=(2, 3)), (3,4,5))
+	@test gradtest(x -> prod(x, dims=1), (3,4,5))
+	@test gradtest(x -> prod(x, dims=1), (3,))
+	@test gradtest(x -> prod(x), (3,4,5))
+	@test gradtest(x -> prod(x), (3,))
+	
+	rzero(dims...) = (x = rand(dims...); x[2]=0; x)
+	@test gradtest(x -> prod(x, dims=(2, 3)), rzero(3,4,5))
+	@test gradtest(x -> prod(x, dims=1), rzero(3,4,5))
+	@test gradtest(x -> prod(x, dims=1), rzero(3,))
+	@test gradtest(x -> prod(x), rzero(3,4,5))
+	@test gradtest(x -> prod(x), rzero(3,))
+	
+	@test gradtest(x -> cumsum(x, dims=2), (3,4,5))
+	@test gradtest(x -> cumsum(x, dims=1), (3,))
+	@test gradtest(x -> cumsum(x), (3,))
+	
+	@test gradtest(x -> cumprod(x, dims=2), (3,4,5))
+	@test gradtest(x -> cumprod(x, dims=1), (3,))
+	@test gradtest(x -> cumprod(x), (3,))
+	@test gradtest(x -> cumprod(x, dims=2), rzero(3,4,5))
+	@test gradtest(x -> cumprod(x, dims=1), rzero(3,))
+	@test gradtest(x -> cumprod(x), rzero(3,))
+
+end
