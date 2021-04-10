351a0b164b4a4cd92da6bc5bcc4e89a49f726987
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-04-10 10:11:15 -0400

    tidy

--- src/ArrayAllez.jl
+++ src/ArrayAllez.jl
@@ -7,26 +7,9 @@ include("inplace.jl")
 @init @require Tracker = "9f7883ad-71c0-57eb-9f7f-b5c9e6d3789c" begin
 	include("inplace-flux.jl")
 	include("prod+cumprod.jl")
-    #= # not sure these work
-    using .Tracker: TrackedReal, TrackedArray, track, @grad
-    *ˡ(x::TrackedReal, y) = track(*ˡ, x, y)
-    *ˡ(x::TrackedArray, y) = track(*ˡ, x, y)
-    *ʳ(x, y::TrackedArray) = track(*ʳ, x, y)
-    *ʳ(x, y::TrackedReal) = track(*ʳ, x, y)
-    @grad *ˡ(A, B) = (data(A) * data(B)),  Δ -> (data(Δ) * data(B)', nothing)
-    @grad *ʳ(A, B) = (data(A) * data(B)),  Δ -> (nothing, data(A)' * data(Δ))
-    =#
 end
 include("inplace-zygote.jl")
 
 include("dropdims.jl")
-
-include("odot.jl")
-
-include("batchmul.jl")
-
-# @init @require OMEinsum = "ebe7aa44-baf0-506c-a96f-8464559b3922" begin
-#     include("omeinsum.jl")
-# end
 
 end
